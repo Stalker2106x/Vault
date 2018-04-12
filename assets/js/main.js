ca49e1613c46fc3169fd22f6614cdf9f28b5a5b6
Maxime <Contact@maximemartens.fr>
2018-04-13 01:40:37 +0200

    added navigation arrows

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,38 +16,55 @@ function loadJSON(file, callback) {
     xobj.send(null);  
  }
 
-// Main
-
-//Getting global app configuration
-loadJSON('data/config.json', function(json){
-    appconfig = JSON.parse(json);
-    document.getElementById('vault-title').innerText = appconfig.title;
-    document.getElementById('vault-caption').innerText = appconfig.caption;
-});
-
-//Rendering app tiles
-loadJSON('data/apps.json', function(json){
-    var data = JSON.parse(json);
-    for (var i in data.apps)
+function updateAppNav() {
+    if (window.innerWidth > document.getElementById('app-container').clientWidth)
     {
-        var appHtml;
-        //Defaults
-        if (!data.apps[i].color) data.apps[i].color = "blue-grey"; else data.apps[i].color = data.apps[i].color.toLowerCase();
-        if (!data.apps[i].textcolor) data.apps[i].textcolor = "white"; else data.apps[i].textcolor = data.apps[i].textcolor.toLowerCase();
-        //Render
-        if (data.apps[i].image) //Image app
-        {
-            appHtml = nunjucks.render('templates/app_image.html', data.apps[i]);
-        }
-        else if (data.apps[i].action && data.apps[i].url) //Standard app
-        {
-            appHtml = nunjucks.render('templates/app_base.html', data.apps[i]);
-        }
-        else //Not an app (note or alert)
-        {
-            appHtml = nunjucks.render('templates/app_panel.html', data.apps[i]);
-        }
-        document.getElementById("app-container").innerHTML += appHtml;
+        if (appCursor > 0) navButtons[0].style.display = "";
+        else navButtons[0].style.display = "none";
+        if (appCursor < appTiles.length - 1) navButtons[1].style.display = "";
+        else navButtons[1].style.display = "none";
     }
-});
+}
 
+// Main
+function loadVault() {
+    return (new Promise(function (resolve, reject) {
+        //Getting global app configuration
+        loadJSON('data/config.json', function(json){
+            appconfig = JSON.parse(json);
+            document.getElementById('vault-title').innerText = appconfig.title;
+            document.getElementById('vault-caption').innerText = appconfig.caption;
+        });
+
+        //Rendering app tiles
+        loadJSON('data/apps.json', function(json){
+            var data = JSON.parse(json);
+            for (var i in data.apps)
+            {
+                var appHtml;
+                //Defaults
+                if (!data.apps[i].color) data.apps[i].color = "blue-grey"; else data.apps[i].color = data.apps[i].color.toLowerCase();
+                if (!data.apps[i].textcolor) data.apps[i].textcolor = "white"; else data.apps[i].textcolor = data.apps[i].textcolor.toLowerCase();
+                //Render
+                if (data.apps[i].image) //Image app
+                {
+                    appHtml = nunjucks.render('templates/app_image.html', data.apps[i]);
+                }
+                else if (data.apps[i].action && data.apps[i].url) //Standard app
+                {
+                    appHtml = nunjucks.render('templates/app_base.html', data.apps[i]);
+                }
+                else //Not an app (note or alert)
+                {
+                    appHtml = nunjucks.render('templates/app_panel.html', data.apps[i]);
+                }
+                document.getElementById("app-container").innerHTML += appHtml;
+            }
+            resolve();
+        });
+    }));
+}
+
+loadVault().then(function() {
+    updateAppNav();
+});
